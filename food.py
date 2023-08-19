39b9c9d8e11d5e6764c06c6cf08a946bebe2f669
Ahmed Faraz <ahmed00faraz@gmail.com>
2023-08-19 20:01:53 +0530

    Add files via upload

--- food.py
+++ food.py
@@ -1,14 +1,12 @@
-import turtle
 from turtle import Turtle
 import random
-turtle.register_shape("apple.gif")
 
 
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.shape("apple.gif")
+        self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
